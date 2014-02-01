f528368e1697f946c4470e1a8a9310dce80afc18
Alex Mingoia <talk@alexmingoia.com>
2014-02-01 13:15:10 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,117 @@
+# koa-resource-router
+
+[![Build Status](https://secure.travis-ci.org/alexmingoia/koa-resource-router.png)](http://travis-ci.org/alexmingoia/koa-resource-router) 
+[![Dependency Status](https://david-dm.org/alexmingoia/koa-resource-router.png)](http://david-dm.org/alexmingoia/koa-resource-router)
+[![NPM version](https://badge.fury.io/js/koa-resource-router.png)](http://badge.fury.io/js/koa-resource-router)
+
+RESTful resource routing for [koa](https://github.com/koajs/koa).
+
+* Rails-like REST resource routing.
+* Use multiple middleware for resource actions.
+* Responds to `OPTIONS` requests with allowed methods.
+* Returns `405 Method Not Allowed` when applicable.
+
+## Installation
+
+Install using [npm](https://npmjs.org):
+
+```sh
+npm install koa-resource-router
+```
+
+## API
+
+### new Resource(path, actions, options)
+
+```javascript
+var app = require('koa')()
+
+var users = new Resource('users', {
+  // GET /users
+  index: function *(next) {
+  },
+  // GET /users/new
+  new: function *(next) {
+  },
+  // POST /users
+  create: function *(next) {
+  },
+  // GET /users/:id
+  show: function *(next) {
+  },
+  // GET /users/:id/edit
+  edit: function *(next) {
+  },
+  // PUT /users/:id
+  update: function *(next) {
+  },
+  // DELETE /users/:id
+  destroy: function *(next) {
+  }
+});
+
+app.use(users.middleware());
+```
+
+### Action mapping
+
+Actions are then mapped accordingly:
+
+```javascript
+GET     /users             ->  index
+GET     /users/new         ->  new
+POST    /users             ->  create
+GET     /users/:user       ->  show
+GET     /users/:user/edit  ->  edit
+PUT     /users/:user       ->  update
+DELETE  /users/:user       ->  destroy
+```
+
+### Overriding action mapping
+
+```javascript
+var users = new Resource('users', actions, {
+  methods: {
+    update: 'PATCH'
+  }
+});
+```
+
+### Top-level resource
+
+Omit the resource name to specify a top-level resource:
+
+```javascript
+var root = new Resource(require('./frontpage'));
+```
+
+Top-level controller actions are mapped as follows:
+
+```javascript
+GET     /          ->  index
+GET     /new       ->  new
+POST    /          ->  create
+GET     /:id       ->  show
+GET     /:id/edit  ->  edit
+PUT     /:id       ->  update
+DELETE  /:id       ->  destroy
+```
+
+### Nesting
+
+Resources can be nested using `resource.add()`:
+
+```javascript
+var forums = new Resource('forums', require('./forum'));
+var threads = new Resource('threads', require('./threads'));
+
+forums.add(threads);
+```
+
+### Multiple middleware
+
+```javascript
+var users = new Resource('users', authorize, actions);
+```
+
+## MIT Licensed
---
+++ test/resource.js
@@ -0,0 +1,154 @@
+/**
+ * Resource tests
+ */
+
+var koa = require('koa')
+  , http = require('http')
+  , request = require('supertest')
+  , Resource = require('../lib/resource')
+  , should = require('should');
+
+describe('Resource', function() {
+  it('creates new resource', function(done) {
+    var resource = new Resource('forums', {
+      index: function *() {},
+      show: function *() {}
+    });
+    resource.should.be.a('object');
+    resource.should.have.property('name', 'forums');
+    resource.should.have.property('id', 'forum');
+    resource.should.have.property('routes');
+    resource.routes.should.be.an.instanceOf(Array);
+    resource.routes.should.have.property(0);
+    resource.routes.should.have.property(1);
+    resource.routes[0].should.have.property('url', '/forums');
+    resource.routes[1].should.have.property('url', '/forums/:forum');
+    done();
+  });
+
+  it('nests resources', function(done) {
+    var app = koa();
+    var forums = new Resource('forums', {
+      index: function *() {}
+    });
+    var threads = new Resource('threads', {
+      index: function *() {},
+      show: function *() {
+        should.exist(this.params);
+        this.params.should.have.property('forum', '54');
+        this.params.should.have.property('thread', '12');
+        this.status = 200;
+      }
+    });
+    forums.add(threads);
+    threads.base.should.equal('/forums/:forum/threads');
+    app.use(threads.middleware());
+    app.use(threads.middleware());
+    request(http.createServer(app.callback()))
+      .get('/forums/54/threads/12')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('options.methods overrides action methods', function(done) {
+    var app = koa();
+    app.use(Resource('users', {
+      update: function *() {
+        this.status = 200;
+      }
+    }, {
+      methods: {
+        update: 'PATCH'
+      }
+    }).middleware());
+    request(http.createServer(app.callback()))
+      .patch('/users/123')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('routes top-level resource', function(done) {
+    var app = koa();
+    app.use(Resource({
+      index: function *() {
+        this.status = 200;
+      }
+    }).middleware());
+    request(http.createServer(app.callback()))
+      .get('/')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('doesn\'t call multiple controller actions', function(done) {
+    var app = koa();
+    var counter = 0;
+    function *increaseCounter() {
+      counter++;
+      this.status = 204;
+    }
+    app.use(Resource('threads', {
+      index: increaseCounter,
+      new: increaseCounter,
+      create: increaseCounter,
+      show: increaseCounter,
+      edit: increaseCounter,
+      update: increaseCounter,
+      destroy: increaseCounter,
+    }).middleware());
+    var server = http.createServer(app.callback());
+    request(server)
+    .get('/threads')
+    .expect(204)
+    .end(function(err, res) {
+      if (err) return done(err);
+      request(server)
+      .get('/threads/new')
+      .expect(204)
+      .end(function(err, res) {
+        if (err) return done(err);
+        request(server)
+        .post('/threads')
+        .expect(204)
+        .end(function(err, res) {
+          if (err) return done(err);
+          request(server)
+          .get('/threads/1234')
+          .expect(204)
+          .end(function(err, res) {
+            if (err) return done(err);
+            request(server)
+            .get('/threads/1234/edit')
+            .expect(204)
+            .end(function(err, res) {
+              if (err) return done(err);
+              request(server)
+              .put('/threads/1234')
+              .expect(204)
+              .end(function(err, res) {
+                if (err) return done(err);
+                request(server)
+                .get('/threads/1234')
+                .expect(204)
+                .end(function(err, res) {
+                  if (err) return done(err);
+                  counter.should.equal(7);
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
